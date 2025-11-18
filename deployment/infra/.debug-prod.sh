5d91f6da17a085e6b74bb937e7ac8bed836d2762
Joe Ipe <joe.ipe@hotmail.com>
2025-11-18 15:30:41 +1100

    update

--- deployment/infra/.debug-prod.sh
+++ deployment/infra/.debug-prod.sh
@@ -1,5 +1,5 @@
 # set the subscription
-export ARM_SUBSCRIPTION_ID="Personal-Sub(1)(57f4859b-8037-4106-ac17-61520b9de19b)"
+export ARM_SUBSCRIPTION_ID="57f4859b-8037-4106-ac17-61520b9de19b"
 
 # set the application / environment
 export TF_VAR_application_name="entra-connect"
