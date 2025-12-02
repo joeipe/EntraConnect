9dd949ddeecbc8bdf4f83c4dba3a394509b9ce18
Joe Ipe <joe.ipe@hotmail.com>
2025-12-02 14:42:07 +1100

    update

--- deployment/infra/main.tf
+++ deployment/infra/main.tf
@@ -52,6 +52,12 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_role_assignment" "terraform_user" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Administrator"
+  principal_id         = "82455d2f-d67d-4648-8267-d5db3b037e40"
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
